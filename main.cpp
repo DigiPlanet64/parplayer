97d2646330a61a3b4ca1789a228ba0b5ca4756b4
DigiPlanet64 <55940246+DigiPlanet64@users.noreply.github.com>
2019-09-29 11:56:59 +0330

    adding git ignores

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 
 #include "GUI/Main/FormMain.h"
 
+
 #include <QtCore/qglobal.h>
 #if QT_VERSION >= 0x050000
 #include <QtWidgets/QApplication>
 #else
 #include <QtGui/QApplication>
 #endif
+
 
 int main(int argc, char *argv[])
 {
